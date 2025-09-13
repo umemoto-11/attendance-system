46ad19551696a6056bc0a02560fc12711b5899cf
umemoto iria <iria.11-15@au.com>
2025-09-13 18:44:12 +0900

    feat: add admin login view scaffold

---
+++ src/resources/views/admins/login.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/login.css') }}">
+@endsection
+
+@section('content')
+<div class="login-form__content">
+    <div class="login-form__heading">
+        <h1>管理者ログイン</h1>
+    </div>
+    <form action="" method="post">
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">メールアドレス</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="email" name="email" value="{{ old('email') }}">
+                </div>
+                <div class="form__error">
+                    @error('email')
+                    {{ $message }}
+                    @enderror
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">パスワード</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="password" name="password">
+                </div>
+                <div class="form__error">
+                    @error('password')
+                    {{ $message }}
+                    @enderror
+                </div>
+            </div>
+        </div>
+        <div class="form__button">
+            <button class="form__button-submit" type="submit">管理者ログインする</button>
+        </div>
+    </form>
+</div>
+@endsection
