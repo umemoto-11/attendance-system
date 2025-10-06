56c8d24deeb4d9914336942b9d1ed9d396652d2f
umemoto iria <iria.11-15@au.com>
2025-10-06 18:07:47 +0900

    fix: make the break_end column nullable in the breaks table

--- src/database/migrations/2025_09_16_165635_create_breaks_table.php
+++ src/database/migrations/2025_09_16_165635_create_breaks_table.php
@@ -17,7 +17,7 @@ class CreateBreaksTable extends Migration
             $table->id();
             $table->foreignId('attendance_id')->constrained()->cascadeOnDelete();
             $table->time('break_start');
-            $table->time('break_end');
+            $table->time('break_end')->nullable();
             $table->timestamps();
         });
     }
