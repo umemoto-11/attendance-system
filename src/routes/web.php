cf886580e1a570f273c3602e48a2766a8af448f5
umemoto iria <iria.11-15@au.com>
2025-10-06 17:41:08 +0900

    feat: add route for attendance page

--- src/routes/web.php
+++ src/routes/web.php
@@ -25,3 +25,8 @@ Route::get('/admin/login', function () {
     return view('admin.login');
 })->name('admin.login');
 Route::post('/admin/login', [CustomLoginController::class, 'store'])->name('admin.login.post');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/attendance', [AttendanceController::class, 'record'])->name('attendance');
+    Route::post('/attendance', [AttendanceController::class, 'store'])->name('attendance.store');
+});
